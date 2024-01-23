1560ef397f7c03585fcee422a75562421c7e3710
michelekluck <140463604+michelekluck@users.noreply.github.com>
2024-01-23 18:15:28 -0300

    endponts

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/employee', (req, res) => {
+    req.body.id=1
+    res.send(req.body)
+})
+
+app.post('/squad', (req, res) => {
+    res.send(req.body)
+})
+
+app.get('/employee/:id', (req, res) => {
+    res.send({
+        "id": req.params.id,
+        "name": "Pedro",
+        "title": "Software Engineer"
+      })
 })
 
 app.listen(port, () => {
